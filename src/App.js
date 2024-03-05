b2622723f1d59d3ecfde0e7c061d40b901784402
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-03-05 04:20:09 +0200

    lil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
+import TG from "./assets/TG.svg"
+import TW from "./assets/TW.svg"
+import lilwayne from "./assets/lilwayne.png"
 
 function App() {
+
+  const ticker = "$YMCMB"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section1">
+        <div className="main">
+          <img alt="logo" className="logo" src={logo}/>
+          <h1 className="heading">Yung Muney Cush Muney Bullioners</h1>
+          <a href="https://t.me/link"><button className="button">BUY {ticker}</button></a>
+          <div className="socialsBlock">
+            <a href="https://t.me/link" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
+            <a href="https://twitter.com/link" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
+          </div>
+          <div className="batjockBlock"><img alt="batjok" className="lilwayne" src={lilwayne}/></div>
+        </div>
+      </div>
     </div>
   );
 }
