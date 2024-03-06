fd8e65dc004df7bf9ae5df2fb80dfae28b18450f
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-03-06 17:53:52 +0200

    asdasd

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
         <div className="main">
           <img alt="logo" className="logo" src={logo}/>
           <h1 className="heading">Yung Muney Cush Muney Bullioners</h1>
-          <a href="https://t.me/link"><button className="button">BUY {ticker}</button></a>
+          <a href="https://t.me/lulwayun"><button className="button">BUY {ticker}</button></a>
           <div className="socialsBlock">
-            <a href="https://t.me/link" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
-            <a href="https://twitter.com/link" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
+            <a href="https://t.me/lulwayun" target="_blank" rel="noopener noreferrer"><img className="social" alt="TG" src={TG} /></a>
+            <a href="https://twitter.com/lulwayun" target="_blank" rel="noopener noreferrer"><img className="social" alt="TW" src={TW} /></a>
           </div>
           <div className="batjockBlock"><img alt="batjok" className="lilwayne" src={lilwayne}/></div>
         </div>
